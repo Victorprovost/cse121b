b93397f678b79db8589833864c4fd5d2af13c5cb
Victorprovost <victorop007@gmail.com>
2024-01-22 19:18:12 +0100

    wk2 work

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,23 +4,37 @@
 
 /* Step 2 - Variables */
 
-
+const fullname = "victor provost";
+const currentyear = new Date().getFullYear(); //"2024"
+const profilepic = "images/victor.png";
 
 
 /* Step 3 - Element Variables */
-
+const nameElement = document.getElementById("name");
+const yearElement = document.querySelector("year");
+const imageElement = document.querySelector("img");
+const foodElement = document.querySelector("food");
 
 
 
 
 /* Step 4 - Adding Content */
-
+nameElement.innerHTML = `<strong>${fullname}</strong>`;
+yearElement.textContent = currentyear;
+imageElement.setAttribute("src", profilepic);
+imageElement.setAttribute("alt", `This is brother ${fullname}. He is a student of BYU-Idaho.`);
 
 
 
 
 
 /* Step 5 - Array */
+let favfoods = ["chicken", "fish", "pork", "beef", "lamb", "vegetables", "bread"];
+
+foodElement.innerHTML = favfoods;
+let newfood = "chocolates";
+favfood.push(newfood);
+foodElement.innerHTML += `<br>${favfoods}`;
 
 
 
