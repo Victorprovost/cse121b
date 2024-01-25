67314f918d7962a2b32aa096baf7b0a69b00f114
Victorprovost <victorop007@gmail.com>
2024-01-25 16:00:04 +0100

    new changes made

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -8,15 +8,11 @@ const fullname = "victor provost";
 const currentyear = new Date().getFullYear(); //"2024"
 const profilepic = "images/victor.png";
 
-
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById("name");
-const yearElement = document.querySelector("year");
+const yearElement = document.querySelector("#year"); 
 const imageElement = document.querySelector("img");
-const foodElement = document.querySelector("food");
-
-
-
+const foodElement = document.querySelector("#food"); 
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullname}</strong>`;
@@ -24,20 +20,13 @@ yearElement.textContent = currentyear;
 imageElement.setAttribute("src", profilepic);
 imageElement.setAttribute("alt", `This is brother ${fullname}. He is a student of BYU-Idaho.`);
 
-
-
-
-
 /* Step 5 - Array */
 let favfoods = ["chicken", "fish", "pork", "beef", "lamb", "vegetables", "bread"];
 
-foodElement.innerHTML = favfoods;
+foodElement.innerHTML = favfoods.join(", ");
 let newfood = "chocolates";
-favfood.push(newfood);
-foodElement.innerHTML += `<br>${favfoods}`;
-
-
-
+favfoods.push(newfood); 
+foodElement.innerHTML += `<br>${favfoods.join(", ")}`; 
 
 
 
